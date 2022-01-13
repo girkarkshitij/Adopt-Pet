800689f75ded62cb14b154f57daceeb1bcc88ce2
Kshitij Girkar <girkarkshitij@gmail.com>
2022-01-13 13:17:49 +0530

    fetch pet details

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,17 +1,29 @@
+import { useEffect, useState } from 'react/cjs/react.development';
+
 const Details = () => {
-  async function getPetDetails() {
-    const res = await fetch(
-      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
-    );
-    console.log(res);
+  const [details, setDetails] = useState({
+    name: '',
+    animal: '',
+    breed: '',
+    description: '',
+    city: '',
+    state: '',
+    images: [],
+  });
+
+  useEffect(() => {
+    const id = location.pathname.split('/')[2];
+    getPetDetails(id);
+  }, []); //eslint-disable-line
+
+  async function getPetDetails(id) {
+    const res = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
+    const json = await res.json();
+    setDetails({ ...json.pets[0] });
+    console.log(details);
   }
 
-  return (
-    <h2>
-      {getPetDetails()}
-      Details
-    </h2>
-  );
+  return <h2>Details</h2>;
 };
 
 export default Details;
